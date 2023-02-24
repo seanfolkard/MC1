cb0efc1002acc886c506ea8916d1a5d43cbea4ad
stanpai78 <stanpai78@gmail.com>
2023-02-24 18:48:21 +0800

    Moved and refactored

---
+++ js/venue-registration.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    let form = document.querySelector("#js-fill");
+    let userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    form.innerHTML += inputGen("firstname", "First Name", "text", userInfo.firstname);
+    form.innerHTML += inputGen("lastname", "Last Name", "text", userInfo.lastname, true);
+    form.innerHTML += inputGen("email", "Email Address", "email", userInfo.email, true);
+    form.innerHTML += inputGen("contact", "Contact Number", "tel", userInfo.contact, true);
+    form.innerHTML += inputGen("dob", "Date of Birth", "date", userInfo.dob, true);
+});
+
+function inputGen(name, display, type, value, linebreak) {
+    let string = "<label for\"" + name + "\"> " + display + " </label><input type=\"" + type + "\" name=\"" + name + "\" value=\"" + value + "\" required>";
+    if (linebreak)
+        string += "<br>";
+    return string;
+}
