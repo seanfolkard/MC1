3557e71c87bb6d6b3afc555cfb3cbdc217d97a3d
stanpai <stanpai78@gmail.com>
2023-03-02 15:02:25 +0800

    Implemented JS for user profile data

--- js/userpro.js
+++ js/userpro.js
@@ -1,20 +1,37 @@
 var savebutton = document.getElementById('savebutton');
 var readonly = true;
-var inputs = document.querySelectorAll('input[type="text"]');
+var inputs = document.querySelectorAll('input');
 
 savebutton.addEventListener('click',function(){
-    
-     for (var i=0; i<inputs.length; i++) {
-     inputs[i].toggleAttribute('readonly');
-     };
+
+    for (var i=0; i<inputs.length; i++) {
+        inputs[i].toggleAttribute('readonly');
+    };
 
     if (savebutton.innerHTML == "edit") {
-      savebutton.innerHTML = "save";
-         } else {
-      savebutton.innerHTML = "edit";
-      }
+        savebutton.innerHTML = "save";
+    } else {
+        savebutton.innerHTML = "edit";
+    }
 
 
 
-     
+
+});
+
+const logout = document.getElementById("donebutton");
+logout.addEventListener("click", (e) => {
+    localStorage.clear();
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    if (localStorage.getItem("userInfo") == null) {
+        window.location.replace("login.html");
+    } else {
+        let userData = JSON.parse(localStorage.getItem("userInfo"));
+        for (var key in userData) {
+            if (userData[key] != "")
+                document.querySelector("#" + key).setAttribute("value", userData[key]);
+        }
+    }
 });
